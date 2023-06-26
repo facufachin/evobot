3216041aa5d3fec39565700ef6d1d5c77b58b654
facufachin <81763565+facufachin@users.noreply.github.com>
2023-06-26 20:03:13 -0300

    Keep Alive v2

--- structs/Bot.ts
+++ structs/Bot.ts
@@ -17,6 +17,22 @@ import { config } from "../utils/config";
 import { i18n } from "../utils/i18n";
 import { MissingPermissionsException } from "../utils/MissingPermissionsException";
 import { MusicQueue } from "./MusicQueue";
+import express, { Request, Response } from 'express';
+
+const server = express();
+
+server.all('/', (req: Request, res: Response) => {
+  res.send('OK');
+});
+
+function keepAlive() {
+  server.listen(3000, () => {
+    console.log('Server is Ready!! ' + Date.now());
+  });
+}
+
+export default keepAlive;
+
 
 export class Bot {
   public readonly prefix = config.PREFIX;
@@ -33,7 +49,6 @@ export class Bot {
       console.log(`${this.client.user!.username} ready!`);
 
       this.registerSlashCommands();
-      this.startKeepAlive();
     });
 
     this.client.on("warn", (info) => console.log(info));
@@ -43,30 +58,71 @@ export class Bot {
   }
 
   private async registerSlashCommands() {
-    // Remaining code for registering slash commands...
+    const rest = new REST({ version: "9" }).setToken(config.TOKEN);
+
+    const commandFiles = readdirSync(join(__dirname, "..", "commands")).filter((file) => !file.endsWith(".map"));
+
+    for (const file of commandFiles) {
+      const command = await import(join(__dirname, "..", "commands", `${file}`));
+
+      this.slashCommands.push(command.default.data);
+      this.slashCommandsMap.set(command.default.data.name, command.default);
+    }
+
+    await rest.put(Routes.applicationCommands(this.client.user!.id), { body: this.slashCommands });
   }
 
   private async onInteractionCreate() {
-    // Remaining code for handling interaction creation...
-  }
+    this.client.on(Events.InteractionCreate, async (interaction: Interaction): Promise<any> => {
+      if (!interaction.isChatInputCommand()) return;
 
-  private startKeepAlive() {
-    const intervalMinutes = 5; // Interval duration in minutes
-    const intervalMilliseconds = intervalMinutes * 60 * 1000;
+      const command = this.slashCommandsMap.get(interaction.commandName);
 
-    setInterval(() => {
-      this.sendKeepAliveRequest();
-    }, intervalMilliseconds);
-  }
+      if (!command) return;
 
-  private async sendKeepAliveRequest() {
-    try {
-      // Send a request to any Discord API endpoint to keep the bot connection alive
-      await this.client.api.gateway.bot();
+      if (!this.cooldowns.has(interaction.commandName)) {
+        this.cooldowns.set(interaction.commandName, new Collection());
+      }
 
-      console.log('Keep-alive request sent successfully.');
-    } catch (error) {
-      console.error('An error occurred while sending the keep-alive request:', error);
-    }
+      const now = Date.now();
+      const timestamps: any = this.cooldowns.get(interaction.commandName);
+      const cooldownAmount = (command.cooldown || 1) * 1000;
+
+      if (timestamps.has(interaction.user.id)) {
+        const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+          const timeLeft = (expirationTime - now) / 1000;
+          return interaction.reply({
+            content: i18n.__mf("common.cooldownMessage", {
+              time: timeLeft.toFixed(1),
+              name: interaction.commandName
+            }),
+            ephemeral: true
+          });
+        }
+      }
+
+      timestamps.set(interaction.user.id, now);
+      setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
+      try {
+        const permissionsCheck: PermissionResult = await checkPermissions(command, interaction);
+
+        if (permissionsCheck.result) {
+          command.execute(interaction as ChatInputCommandInteraction);
+        } else {
+          throw new MissingPermissionsException(permissionsCheck.missing);
+        }
+      } catch (error: any) {
+        console.error(error);
+
+        if (error.message.includes("permissions")) {
+          interaction.reply({ content: error.toString(), ephemeral: true }).catch(console.error);
+        } else {
+          interaction.reply({ content: i18n.__("common.errorCommand"), ephemeral: true }).catch(console.error);
+        }
+      }
+    });
   }
 }
