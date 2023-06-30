03fbd8693de1d8df3bbcf76c8d74f168ba511ff4
facufachin <81763565+facufachin@users.noreply.github.com>
2023-06-30 18:46:09 -0300

    Update keepAlive.ts

--- structs/keepAlive.ts
+++ structs/keepAlive.ts
@@ -10,6 +10,16 @@ function keepAlive() {
   server.listen(3000, () => {
     console.log('Server is Ready!! ' + Date.now());
   });
+
+  server.on('error', (err: Error) => {
+    console.error('Server error:', err.message);
+    setTimeout(() => {
+      server.listen(3000, () => {
+        console.log('Server is Ready!! ' + Date.now());
+      });
+    }, 5000);
+  });
 }
+
 
 export default keepAlive;
